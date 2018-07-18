32396b61e5655f433a45555e21436cfb5714d225
Marek Kotowski <marek.kotowski87@gmail.com>
2018-07-18 22:56:56 +0200

    video section with autoplay

--- GlozzomMultiPage/wwwroot/js/script.js
+++ GlozzomMultiPage/wwwroot/js/script.js
@@ -6,3 +6,16 @@ $('.carousel').carousel({
     interval: 6000, 
     pause: 'hover'
 });
+
+//video play
+$(function () {
+    $(".video").click(function () {
+        var theModal = $(this).data("target"),
+            videoSRC = $(this).attr("data-video"),
+            videoSRCauto = videoSRC + "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
+        $(theModal + ' iframe').attr('src', videoSRCauto);
+        $(theModal + ' button.close').click(function () {
+            $(theModal + ' iframe').attr('src', videoSRC);
+        });
+    });
+});
